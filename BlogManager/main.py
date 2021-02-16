79c3c9d1c8aa079e640521e8287949effd58a106
hyaline <1097611989@qq.com>
2021-02-16 21:25:46 +0800

    1代码重构，精简代码 2 增加进度条

--- BlogManager/main.py
+++ BlogManager/main.py
@@ -3,22 +3,34 @@ import re
 import codecs
 import requests
 import time
+import sys
 
 img_dir = '..\\images'
-blog_dir = '..\\blogs'
-if __name__ == '__main__':
-    print("图片归类开始")
-    init_time = time.strftime("%Y-%m-%d, %H:%M:%S", time.localtime())
-    print('当前时间' + init_time)
-    isExists = os.path.exists(img_dir)
-    if not isExists:
-        os.makedirs(img_dir)
-        print('create ' + img_dir)
-    isExists = os.path.exists(blog_dir)
-    if not isExists:
-        print('create ' + blog_dir)
-        os.makedirs(blog_dir)
+blog_dir = '..\\blogs'''
 
+
+def download_bar(num, sum):
+    percent = num / sum
+    if num != sum - 1:
+        sys.stdout.write("\r{0}{1}".format("||" * (num + 1), '%.2f%%' % (percent * 100)))
+        sys.stdout.flush()
+    else:
+        sys.stdout.write("\r{0}{1}".format("||" * (num + 1), '%.2f%%' % (100)))
+        sys.stdout.flush()
+        print("\n" + "下载完成")
+
+
+def create_dir(path):
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+        print('create folder :' + path)
+    else:
+        print('the folder already exists')
+
+
+def get_all_md_path(path):
+    md_paths = []
     list = os.listdir(blog_dir)  # 列出文件夹下所有的目录与文件
     print('共发现%d个文件\n' % len(list))
     for i in range(0, len(list)):
@@ -26,9 +38,23 @@ if __name__ == '__main__':
         # if os.path.isfile(path):
         # # 你想对文件的操作
         print('路径为  ：' + path)
+        md_paths.append(path)
+    return md_paths
 
+
+if __name__ == '__main__':
+    print("图片归类开始")
+    init_time = time.strftime("%Y-%m-%d, %H:%M:%S", time.localtime())
+    print('当前时间' + init_time)
+    create_dir(img_dir)
+    create_dir(blog_dir)
+
+    md_paths = get_all_md_path(blog_dir)
+    for path in md_paths:
         input_file = codecs.open(path, mode="r", encoding="utf-8")
         text = input_file.read()
+        input_file.close()
+        # 匹配标题
         # print(text)
         ex = 'blogs\\\\.*?md'
         title = re.findall(ex, path, re.DOTALL)
@@ -36,30 +62,22 @@ if __name__ == '__main__':
         title = title[0]
         title = title[6:title.index('.')]
         print(title)
+        # 匹配网址
         ex = '\[.*?]\(http.*?[" )]'
-        lists = re.findall(ex, text)
-        print(lists)
-        input_file.close()
-        for i, l in enumerate(lists):
-            lists[i] = l[l.index('(') + 1:len(l) - 1]
-        print(lists)
-        isExists = os.path.exists(img_dir + '\\' + title)
-        if not isExists:
-            print('create ' + img_dir + '\\' + title)
-            os.makedirs(img_dir + '\\' + title)
+        img_paths = re.findall(ex, text)
+        print(img_paths)
+        for i, l in enumerate(img_paths):
+            img_paths[i] = l[l.index('(') + 1:len(l) - 1]
+        print(img_paths)
+
         img_root_dir = img_dir + '\\' + title
+        create_dir(img_root_dir)
+
         print('正在下载图片')
-        for u, url in enumerate(lists):
-            print('第%d张' % u)
-            time.sleep(0.1)
+        for u, url in enumerate(img_paths):
+            # print('第%d张' % u)
+            download_bar(u, len(img_paths))
             img_data = requests.get(url=url).content
             with open(img_root_dir + '\\%d.png' % u, 'wb') as fp:
                 fp.write(img_data)
                 fp.close()
-        print('下载完成')
-        # with open(img_dir+'\\'+)
-#  print('此文章不存在')
-#         init_time = time.strftime("%B %d, %Y", time.localtime())
-# with open(dirPath + '/' + summary_txt_file, 'w') as fp:
-#     fp.writelines([file_name + '\n', summary_paragraph, init_time])
-#     fp.close()

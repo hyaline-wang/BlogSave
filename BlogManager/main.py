ad806538f1e1e94d62ad4d30e19cf7867ee67c90
hyaline <1097611989@qq.com>
2021-02-16 22:52:56 +0800

    增加本地版md 和web版md

--- BlogManager/main.py
+++ BlogManager/main.py
@@ -7,6 +7,8 @@ import sys
 
 img_dir = '..\\images'
 blog_dir = '..\\blogs'''
+blog_web_dir = '..\\webBlogs'
+blog_local_dir = '..\\localBlogs'
 
 
 def download_bar(num, sum):
@@ -26,7 +28,7 @@ def create_dir(path):
         os.makedirs(path)
         print('create folder :' + path)
     else:
-        print('the folder already exists')
+        print('the folder already exists'+'(%s)'%(path))
 
 
 def get_all_md_path(path):
@@ -58,26 +60,48 @@ if __name__ == '__main__':
         # print(text)
         ex = 'blogs\\\\.*?md'
         title = re.findall(ex, path, re.DOTALL)
-        print(title)
+        # print(title)
         title = title[0]
         title = title[6:title.index('.')]
         print(title)
         # 匹配网址
-        ex = '\[.*?]\(http.*?[" )]'
+        ex = '!\[.*?]\(http.*?[" )]'
         img_paths = re.findall(ex, text)
-        print(img_paths)
+        # print(img_paths)
         for i, l in enumerate(img_paths):
             img_paths[i] = l[l.index('(') + 1:len(l) - 1]
         print(img_paths)
 
+        # TODO 匹配本地地址
+
         img_root_dir = img_dir + '\\' + title
         create_dir(img_root_dir)
-
+        # 增加web版 增加本地版
+        web_md_text = text
+        local_md_text = text
         print('正在下载图片')
         for u, url in enumerate(img_paths):
             # print('第%d张' % u)
+            web_url = "https://github.com/ChasingTheDreamOfLoad/BlogSave/blob/main/images/"
+            web_md_text = re.sub(url, web_url + str(u) + '.png', web_md_text)
+            # print(local_md_text)
+            # print(url)
+            # print(img_root_dir + '\\'+str(u)+'.png')
+            local_md_text = re.sub(url, '..\\\\images' + '\\\\' + title + '\\\\' + str(u) + '.png', local_md_text)
+
             download_bar(u, len(img_paths))
             img_data = requests.get(url=url).content
             with open(img_root_dir + '\\%d.png' % u, 'wb') as fp:
                 fp.write(img_data)
                 fp.close()
+
+        create_dir(blog_web_dir)
+        output_file = codecs.open(blog_web_dir + '\\' + title + '.md', mode="w", encoding="utf-8")
+        output_file.write(web_md_text)
+        output_file.close()
+
+        create_dir(blog_local_dir)
+        output_file = codecs.open(blog_local_dir + '\\' + title + '.md', mode="w", encoding="utf-8")
+        output_file.write(local_md_text)
+        output_file.close()
+    print("updata complete")
